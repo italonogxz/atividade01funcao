e2895588a717e9443affaa6838af07919b7f800f
italonogueira <147431079+italonogxz@users.noreply.github.com>
2023-11-21 16:46:47 -0300

    Add files via upload

---
+++ italo.py
@@ -0,0 +1,6 @@
+nome = str(input("digite o nome da cidade: "))
+nomemenor = nome.lower()
+if nomemenor.startswith ("santo"):
+    print("o nome dessa cidade tem santo no nome")
+else:
+    print("o nome dessa cidade não tem santo no nome")
